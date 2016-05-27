95f12839c8d6df9951b0353677602cb1000f574d
clverpanda <543833065@qq.com>
2016-05-27 12:59:18 +0800

    the end

--- src/org/service/articleService.java
+++ src/org/service/articleService.java
@@ -1,20 +1,10 @@
 package org.service;
 
-import org.model.articlePublishModel;
-import org.model.articleViewModel;
-import org.model.indexViewModel;
-import org.vo.UserEntity;
-
 import java.util.List;
 
 /**
- * Created by miaol on 2016/5/27 0027.
+ * Created by wcy on 2016/5/26.
  */
-public interface articleService
-{
-    boolean publishArticle(UserEntity author, articlePublishModel article);
-    List<indexViewModel> getIndexArticles();
-    articleViewModel getArticle(int articleId);
-    List<indexViewModel> getPersonalArticles(int userId);
-    void deleteArticle(int articleId);
+public interface ArticleService {
+    List getArticle();
 }
--- src/org/service/impl/articleServiceImpl.java
+++ src/org/service/impl/articleServiceImpl.java
@@ -1,94 +1,22 @@
 package org.service.impl;
 
-import org.dao.getArticleDao;
-import org.dao.publishDao;
-import org.model.articlePublishModel;
-import org.model.articleViewModel;
-import org.model.indexViewModel;
-import org.service.articleService;
+import org.dao.InitializeDao;
+import org.service.ArticleService;
 import org.springframework.stereotype.Service;
-import org.vo.ArticleEntity;
-import org.vo.UserEntity;
-
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by miaol on 2016/5/27 0027.
+ * Created by wcy on 2016/5/26.
  */
 @Service
-public class articleServiceImpl implements articleService
-{
+public class ArticleServiceImpl implements ArticleService {
     @Resource
-    private publishDao publishDao;
-    @Resource
-    private getArticleDao getArticleDao;
+    private InitializeDao initializeDao;
 
     @Override
-    public boolean publishArticle(UserEntity author, articlePublishModel article)
-    {
-        return this.publishDao.doPublish(author, article);
-    }
-
-    @Override
-    public List<indexViewModel> getIndexArticles()
-    {
-        List<indexViewModel> indexViewModelList = new ArrayList<>();
-        for (ArticleEntity article:getArticleDao.doGetArticle())
-        {
-            indexViewModel tempIndexViewModel = new indexViewModel();
-            tempIndexViewModel.setArticleTitle(article.getTitle());
-            tempIndexViewModel.setArticleAuthor(article.getAuthor().getUsername());
-            tempIndexViewModel.setArticleImgPath(article.getImageUrl());
-            tempIndexViewModel.setArticlePublishDate(article.getPublishtime().toString());
-            tempIndexViewModel.setArticleCommentCount(0);
-            tempIndexViewModel.setCollected(false);
-            tempIndexViewModel.setArticleLabel("");
-            tempIndexViewModel.setArticleId(article.getArticleId());
-            indexViewModelList.add(tempIndexViewModel);
-        }
-        return indexViewModelList;
-    }
-
-    @Override
-    public articleViewModel getArticle(int articleId)
-    {
-        ArticleEntity article = getArticleDao.doGetSpecificArticle(articleId);
-        articleViewModel articleView = new articleViewModel();
-        articleView.setArticleTitle(article.getTitle());
-        articleView.setArticleContent(article.getContent());
-        articleView.setArticlePublishTime(article.getPublishtime().toString());
-        articleView.setArticleAuthor(article.getAuthor().getUsername());
-        return articleView;
-    }
-
-    @Override
-    public List<indexViewModel> getPersonalArticles(int userId)
-    {
-        List<indexViewModel> indexViewModelList = new ArrayList<>();
-        for (ArticleEntity article:getArticleDao.doGetArticle())
-        {
-            if (article.getAuthor().getUserid() == userId) {
-                indexViewModel tempIndexViewModel = new indexViewModel();
-                tempIndexViewModel.setArticleTitle(article.getTitle());
-                tempIndexViewModel.setArticleAuthor(article.getAuthor().getUsername());
-                tempIndexViewModel.setArticleImgPath(article.getImageUrl());
-                tempIndexViewModel.setArticlePublishDate(article.getPublishtime().toString());
-                tempIndexViewModel.setArticleCommentCount(0);
-                tempIndexViewModel.setCollected(false);
-                tempIndexViewModel.setArticleLabel("");
-                tempIndexViewModel.setArticleId(article.getArticleId());
-                indexViewModelList.add(tempIndexViewModel);
-            }
-        }
-        return indexViewModelList;
-    }
-
-    @Override
-    public void deleteArticle(int articleId)
-    {
-        publishDao.doDelete(articleId);
+    public List getArticle(){
+        return this.initializeDao.getArticle();
     }
 }
